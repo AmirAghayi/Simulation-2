c47fa5083a29e94d72334f6db9c5b5881efc7cd0
samskeen <sskeen9@gmail.com>
2019-03-27 19:28:28 -0600

    getAll-house

--- src/Components/House/House.js
+++ src/Components/House/House.js
@@ -6,11 +6,11 @@ export default function house(props) {
 
   return (
     <div className="House">
-      <h1>{house.property_name}</h1>
-      <h2>{house.address}</h2>
-      <h2>{house.city}</h2>
-      <h2>{house.state}</h2>
-      <h2>{house.zip}</h2>
+      <h1>Property Name: {house.property_name}</h1>
+      <h2>Address: {house.address}</h2>
+      <h2>City: {house.city}</h2>
+      <h2>State: {house.state}</h2>
+      <h2>Zip Code: {house.zip}</h2>
     </div>
   );
 }
